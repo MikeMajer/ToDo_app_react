9bc24e98c135c17950439cfae5ea58d1af591bf2
Michal Majer <majer.michal@outlook.com>
2019-05-03 12:41:18 +1000

    Updates

---
+++ precache-manifest.6cae602962704470fbefd5c4acfb9d8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "822ae3ad11cd85650e949e727c99c590",
+    "url": "/ToDo_app_react/index.html"
+  },
+  {
+    "revision": "c60585e65b78e1ec87c7",
+    "url": "/ToDo_app_react/static/css/main.f0df82b2.chunk.css"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/ToDo_app_react/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "c60585e65b78e1ec87c7",
+    "url": "/ToDo_app_react/static/js/main.47a030dc.chunk.js"
+  },
+  {
+    "revision": "ba2593df675f216e7c91",
+    "url": "/ToDo_app_react/static/js/runtime~main.222fabd5.js"
+  }
+]);
